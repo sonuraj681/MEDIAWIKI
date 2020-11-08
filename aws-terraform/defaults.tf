a53cf9607ba190bda0c4e765311fcfa3644ef15c
sonuraj681 <71784451+sonuraj681@users.noreply.github.com>
2020-11-09 03:44:27 +0530

    Add files via upload

--- aws-terraform/defaults.tf
+++ aws-terraform/defaults.tf
@@ -5,14 +5,14 @@
 # Region
 variable "region" {
   type = string
-  default =  "us-west-1"
+  default =  "us-west-2"
 }
 
  # Availability Zones
  
 variable "azs" {
   type    = list(string)
-  default = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  default = ["us-west-2a", "us-west-2b", "us-west-2c"]
 }
 
 variable "keyname" {
